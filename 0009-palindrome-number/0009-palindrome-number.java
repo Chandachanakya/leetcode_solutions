d3e3b6ded04433f5991dfc034c986180cd5e6259
Chanakya <165761706+Chandachanakya@users.noreply.github.com>
2026-03-02 21:46:49 +0530

    Time: 5 ms (77.96%), Space: 44 MB (99.98%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+      
+        if(x<0){
+            return false;
+        }
+         int n=x;
+         int r=0;
+            while(n>0){
+                r=r*10+(n%10);
+                n=n/10;
+            }
+                return (x==r);
+    }
+}
